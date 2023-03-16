cc5da6999534bf54fb021e1e42d658c83372ffe3
RobynSpaulding <robynm27@gmail.com>
2023-03-16 14:19:42 -0700

    styles BookingsIndex with bootstrap cards and lists

--- src/BookingsIndex.jsx
+++ src/BookingsIndex.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export function BookingsIndex(props) {
   const [bookings, setBookings] = useState([]);
@@ -13,25 +16,30 @@ export function BookingsIndex(props) {
   useEffect(handleIndexBookings, []);
 
   return (
-    <div>
+    <div id="bookings-index">
       <h1>All Bookings</h1>
-      {bookings.map((booking) => (
-        <div key={booking.id}>
-          <div>Yoga Instructor: {booking.yogi_name}</div>
-          <div>Date: {booking.date}</div>
-          <div>
-            Time: {booking.start_time}-{booking.end_time}
-          </div>
-          <div>Total due at end of event: ${booking.total_price}</div>
-          <div>
-            Address: {booking.address}, {booking.city}, {booking.state}
-            <br /> Event type: {booking.event_type}
-            <br /> In person: {booking.in_person.toString()}
-            <br /> Paid: {booking.paid.toString()}
-          </div>
-          <br />
-        </div>
-      ))}
+      <Row xs={2} md={3} className="g-4">
+        {bookings.map((booking) => (
+          <Col key={booking.id}>
+            <Card style={{ width: "20rem" }}>
+              <div class="card-header">Yoga Instructor: {booking.yogi_name}</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Date: {booking.date}</li>
+                <li class="list-group-item">
+                  Time: {booking.start_time}-{booking.end_time}
+                </li>
+                <li class="list-group-item">Total due at end of event: ${booking.total_price}</li>
+                <li class="list-group-item">
+                  Address: {booking.address}, {booking.city}, {booking.state}
+                </li>
+                <li class="list-group-item">Event type: {booking.event_type} </li>
+                <li class="list-group-item">In person: {booking.in_person.toString()} </li>
+                <li class="list-group-item">Paid: {booking.paid.toString()} </li>
+              </ul>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
